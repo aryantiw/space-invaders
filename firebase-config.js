bc9d62e0c4b50866ccb63c7ce7744b094cc3285b
Aryan <77456196+aryantiw@users.noreply.github.com>
2026-09-08 19:40:05 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: 'AIzaSyABjm86ClFvEI88kJkNsVczkYLtgrLvuRA',
+  authDomain: 'ufo-shooter-1f6f4.firebaseapp.com',
+  projectId: 'ufo-shooter-1f6f4',
+  storageBucket: 'ufo-shooter-1f6f4.firebasestorage.app',
+  messagingSenderId: '687843063094',
+  appId: '1:687843063094:web:66a3df73ee744321932f97',
+  measurementId: 'G-7MD4H42HVC'
+};
